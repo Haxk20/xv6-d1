aabf188dc32f81af47ae3ecb3afc19163ff7aebf
Martin Botka <martin.botka@somainline.org>
2022-11-23 23:28:24 +0100

    plic.c: Setup PLIC priorities correctly
    Signed-off-by: Martin Botka <martin.botka@somainline.org>

--- kernel/plic.c
+++ kernel/plic.c
@@ -18,22 +18,21 @@ plicinit(void)
 void
 plicinithart(void)
 {
-  int hart = cpuid();
-  
   // set enable bits for this hart's S-mode
-  // for the uart and virtio disk.
-  *(uint32*)PLIC_SENABLE(hart) = (1 << UART0_IRQ));
+  // for the uart
+  *(uint32*)PLIC_SENABLE = (1 << UART0_IRQ);
+  *(uint32*)(PLIC_PRIORITY + (UART0_IRQ * 4)) = 0x1f;
 
-  // set this hart's S-mode priority threshold to 0.
-  *(uint32*)PLIC_SPRIORITY(hart) = 0;
+  // set this hart's S-mode and M mode priority threshold to 0.
+  *(uint32*)PLIC_MPRIORITY = 0x0;
+  *(uint32*)PLIC_SPRIORITY = 0x0;
 }
 
 // ask the PLIC what interrupt we should serve.
 int
 plic_claim(void)
 {
-  int hart = cpuid();
-  int irq = *(uint32*)PLIC_SCLAIM(hart);
+  int irq = *(uint32*)PLIC_SCLAIM;
   return irq;
 }
 
@@ -41,6 +40,5 @@ plic_claim(void)
 void
 plic_complete(int irq)
 {
-  int hart = cpuid();
-  *(uint32*)PLIC_SCLAIM(hart) = irq;
+  *(uint32*)PLIC_SCLAIM = irq;
 }
